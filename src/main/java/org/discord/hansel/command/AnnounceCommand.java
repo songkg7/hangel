47705504728fc7a7c144a78dd1405a3430930c5c
kgsong <kyunggeun.song@seavantage.com>
2023-06-06 01:35:20 +0900

    refactor: add sample button for an announcement

--- src/main/java/org/discord/hansel/command/AnnounceCommand.java
+++ src/main/java/org/discord/hansel/command/AnnounceCommand.java
@@ -1,12 +1,19 @@
 package org.discord.hansel.command;
 
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.component.ActionRow;
+import discord4j.core.object.component.Button;
 import discord4j.rest.util.AllowedMentions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
 public class AnnounceCommand implements SlashCommand {
+
+    @Value("${google.spreadsheet-url}")
+    public String url;
+
     @Override
     public String getName() {
         return "announce";
@@ -16,6 +23,7 @@ public class AnnounceCommand implements SlashCommand {
     public Mono<Void> handle(ChatInputInteractionEvent event) {
         return event.reply()
                 .withAllowedMentions(AllowedMentions.builder().parseType(AllowedMentions.Type.EVERYONE_AND_HERE).build())
+                .withComponents(ActionRow.of(Button.link(url, "snack-request-sheet")))
                 .withContent("@everyone\n간식 신청 공지 테스트");
     }
 }

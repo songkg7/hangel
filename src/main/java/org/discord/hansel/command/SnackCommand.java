adb43afce58159bb0f8c3752610efdf315bfc668
kgsong <kyunggeun.song@seavantage.com>
2023-06-09 22:50:00 +0900

    feat: update spreadsheet

---
+++ src/main/java/org/discord/hansel/command/SnackCommand.java
@@ -0,0 +1,63 @@
+package org.discord.hansel.command;
+
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.command.ApplicationCommandInteractionOption;
+import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.util.Color;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.discord.hansel.gcloud.SheetsService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SnackCommand implements SlashCommand {
+
+    @Value("${google.spreadsheet.id}")
+    private String spreadsheetId;
+
+    private final SheetsService sheetsService;
+
+    @Override
+    public String getName() {
+        return "snack";
+    }
+
+    @Override
+    public Mono<Void> handle(ChatInputInteractionEvent event) {
+        String snack = getOption(event, "snack");
+        String link = getOption(event, "link");
+        String name = getOption(event, "name");
+
+        try {
+            sheetsService.updateSnack(spreadsheetId, name, snack, link);
+        } catch (IOException e) {
+            log.error("Error while updating sheets", e);
+            throw new RuntimeException(e);
+        }
+
+        return event.reply()
+                .withEmbeds(EmbedCreateSpec.builder()
+                        .addField("link", link, true)
+                        .color(Color.WHITE)
+                        .description(snack)
+                        .url(link)
+                        .build()
+                )
+                .withEphemeral(false)
+                .withContent("간식 신청 완료!");
+    }
+
+    private static String getOption(ChatInputInteractionEvent event, String link) {
+        return event.getOption(link)
+                .flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asString)
+                .orElseThrow();
+    }
+}
--- src/main/java/org/discord/hansel/gcloud/SheetsService.java
+++ src/main/java/org/discord/hansel/gcloud/SheetsService.java
@@ -3,17 +3,21 @@ package org.discord.hansel.gcloud;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.time.LocalDate;
+import java.util.List;
 
 @Slf4j
 @Service
 public class SheetsService {
 
     private static final String APPLICATION_NAME = "Hansel Discord Bot for Snack Request";
+    private static final String START_RANGE = "B2";
 
     public Sheets getSheets() {
         try {
@@ -26,4 +30,8 @@ public class SheetsService {
         }
     }
 
+    public void updateSnack(String spreadsheetId, String username, String snack, String link) throws IOException {
+        List<List<Object>> values = List.of(List.of(LocalDate.now().toString(), username, snack, link));
+        getSheets().spreadsheets().values().append(spreadsheetId, START_RANGE, new ValueRange().setValues(values)).setValueInputOption("USER_ENTERED").execute();
+    }
 }
